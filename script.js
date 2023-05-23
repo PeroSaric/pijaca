8cbcca6e5dff3516601bff8ce13a2fd842a931b4
Pero Šarić <41336162+PeroSaric@users.noreply.github.com>
2023-05-23 09:41:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function addToCart(element) {
+    let mainEl = element.closest(".single-item");
+    let price = mainEl.querySelector(".price").innerText;
+    let name = mainEl.querySelector("h3").innerText;
+    let quantity = mainEl.querySelector("input").value;
+    let cartItems = document.querySelector(c)
+    console.log(quantity);
+    
+}
+
+
+ 
